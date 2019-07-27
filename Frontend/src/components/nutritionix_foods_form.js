c722975ac1dfede5dc3844cf6aba78290558b788
Will Jones <willjones2010@gmail.com>
2019-07-27 13:41:49 -0400

    added in a fade in search results card on the consumptions page. need to fill in the results from state

--- Frontend/src/components/nutritionix_foods_form.js
+++ Frontend/src/components/nutritionix_foods_form.js
@@ -1,15 +1,33 @@
 import React, { Component } from 'react';
 import { Form, FormInput, FormGroup, FormSelect } from "shards-react";
-import { Button } from "shards-react";
+import { Button, Fade } from "shards-react";
 import { connect } from "react-redux"
 import { Redirect } from "react-router-dom";
 import { fetchUser } from '../actions/user_actions'
+import { Card, ListGroup} from 'react-bootstrap'
+
 
 class NutritionixForm extends Component {
+    constructor(props) {
+        super(props);
+        this.toggle = this.toggle.bind(this);
+        this.state = {
+          visible: false,
+          redirect: null
+        };
+      }
     
-    state = {
-        redirect: null
-    }
+      toggle() {
+        this.setState({
+          visible: !this.state.visible
+        });
+      }
+
+    // fix the redirect none currently upon the search
+    // do i need to persist the information through refresh for consumptions?
+    // not if i plan to have the user to chose to save that to the backend when looking up the search information 
+    // and save as a consumption
+
 
     handleSearchNutrionixFoods = (e) => {
         e.preventDefault()
@@ -26,7 +44,10 @@ class NutritionixForm extends Component {
 		  	if (res.foods[0]) {
                 this.props.dispatch({type: "UPDATE_CONSUMPTION", foodArr: res.foods})
                 
-		  	}
+              }
+        this.setState({ redirect: <Redirect to='/consumptions' /> })
+        this.toggle()
+
 		//   	else if(res.errors)
 		//   		this.setState({ errors: res.errors })
           })
@@ -35,7 +56,7 @@ class NutritionixForm extends Component {
 
     render() { 
         return ( 
-        
+        <div>
         <Form onSubmit={(e) => this.handleSearchNutrionixFoods(e)} style={{position: "absolute", top: '60px', width: 300, height: 550, margin: '20px'}}>
             {this.state.redirect}
 
@@ -43,7 +64,27 @@ class NutritionixForm extends Component {
                 <FormInput name="nutritionixSearch" id="#nutritionixSearch" placeholder="Search Nutrionix Foods!" />
             </FormGroup>
                 <Button className="mb-2" type="submit">Submit</Button>
-            </Form> );
+            </Form> 
+
+            <Fade in={this.state.visible}>
+                    <Card id="nutritionixSearchInfoCard" style={{position: 'relative', left:'100px', top: '100px', width: '200px'}}>
+                        <Card.Body>
+                            <Card.Title>Search Results</Card.Title>
+                                <Card.Text>
+                                <ListGroup>
+                                    <ListGroup.Item>info</ListGroup.Item>
+                                </ListGroup>
+                                </Card.Text>
+                                <Button onClick={this.toggle} className="mb-2">
+                                    Close
+                                </Button>
+                        </Card.Body>
+                    </Card>   
+                </Fade>
+        </div>
+            );
+
+        
     }
 }
  
